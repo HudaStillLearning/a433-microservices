ad5969f40c69f381848b8a42bc3d000cab25f525
HudaStillLearning <muhammadhuda537@gmail.com>
2024-09-11 16:36:12 +0700

    update file

--- build_push_image.sh
+++ build_push_image.sh
@@ -5,12 +5,12 @@
 docker build -t item-app:v1 . 
 # menampilkan daftar image yang ada di local
 docker images
-# menyesuaikan nama image dengan format Docker Hub
-docker tag item-app:v1 k1zuko/item-app:v1
+# menyesuaikan nama image dengan format Docker Hub / github
+docker tag item-app:v1 ghcr.io/hudastilllearning/item-app:v1
 
 # jika sudah login ke Docker Hub via terminal, kode dibawah ini tidak perlu dijalankan
 # export PASSWORD_DOCKER_HUB="mypasswd"
 # echo $PASSWORD_DOCKER_HUB | docker login --username k1zuko --password-stdin
 
-# meng-upload image ke Docker Hub
-docker push k1zuko/item-app:v1
+# meng-upload image ke Docker Hub / github
+docker push ghcr.io/hudastilllearning/item-app:v1
